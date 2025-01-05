cfcd1a17772104f5a5bc253a9f2ef63a5174b718
dotarsojat69 <tdeath457@gmail.com>
2025-01-06 02:01:59 +0700

    feat: fix ui

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -16,7 +16,7 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           ref={ref}
           type={showPassword ? "text" : type}
-          className={`w-full bg-gray-800/50 rounded-lg px-4 py-3 text-white placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className} ${
+          className={`w-full bg-input rounded-lg px-5 py-4 text-white text-sm placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className} ${
             error ? "ring-2 ring-red-500" : ""
           }`}
           {...props}
@@ -27,7 +27,11 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
             onClick={() => setShowPassword(!showPassword)}
             className="absolute right-3 top-1/2 -translate-y-1/2"
           >
-            {showPassword ? <Eye color="grey" /> : <EyeOffIcon color="grey" />}
+            {showPassword ? (
+              <Eye size={20} color="grey" />
+            ) : (
+              <EyeOffIcon size={20} color="grey" />
+            )}
           </button>
         )}
         {error && <p className="mt-1 text-sm text-red-500">{error}</p>}
